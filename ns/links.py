6a043497dea86edca032dba07fdeb8c77056ff01
Pavol Travnik <pavol.travnik@gmail.com>
2018-05-19 17:23:50 +0200

    new parser najvyssi soud

--- ns/links.py
+++ ns/links.py
@@ -13,16 +13,21 @@ import os
 import multiprocessing
 import calendar
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--month", help="start month", type=int)
 parser.add_argument("--year", help="start year", type=int)
 args = parser.parse_args()
 
-
 path="ns/documents"
 if not os.path.exists(path):
 	os.makedirs(path)
+
+from os import listdir
+from os.path import isfile, join
+saved_documents = [os.path.splitext(f)[0] for f in listdir("ns/documents") if isfile(join("ns/documents", f))]
+
 
 def parse_month(parse_year,parse_month):
 	for day in range(1, calendar.monthrange(parse_year,parse_month)[1]):
@@ -31,7 +36,17 @@ def parse_month(parse_year,parse_month):
 
 def parse_decisions(year,month,day):
 	address = "http://nsoud.cz/Judikatura/judikatura_ns.nsf/$$WebSearch1?SearchView&Query=%5Bdatum_predani_na_web%5D%3E%3D{0}%2F{1}%2F{2}%20AND%20%5Bdatum_predani_na_web%5D%3C%3D{3}%2F{4}%2F{5}&SearchOrder=2&SearchMax=0&Start=1&Count=1000&pohled=1".format(str(day),str(month),str(year),str(day),str(month),str(year))
-	r = requests.get(address,allow_redirects=True)
+	count = 0
+	while (count < 10):
+		count = count + 1
+		try:
+			r = requests.get(address,allow_redirects=True)
+		except Exception as e:
+			print("{} - {}".format(matchObj, e))
+		if q.status == 200:
+			break
+		else:
+			time.sleep(5)
 	root = lxml.html.fromstring(r.text)
 	links = root.xpath("//a/@href[starts-with(.,'/Judikatura/judikatura_ns.nsf/WebSearch')]")
 	if len(links) > 990:
@@ -42,7 +57,7 @@ def parse_decisions(year,month,day):
 		with open("ns/links.txt", "a") as myfile:
 			myfile.write(link+"\n")
 
-	pool = multiprocessing.Pool(processes=8)
+	pool = multiprocessing.Pool(processes=4)
 	pool_outputs = pool.map(get_content,links)
 	pool.close()
 	pool.join()
@@ -51,8 +66,18 @@ def parse_decisions(year,month,day):
 def get_content(link):
 	try:
 		matchObj = re.match( r'\/Judikatura\/judikatura_ns\.nsf\/WebSearch\/(.*?)\?openDocument', link, re.M|re.I)
-		if matchObj:
-			q = requests.get("http://nsoud.cz"+link,allow_redirects=True)
+		if matchObj and matchObj not in saved_documents:
+			count = 0
+			while (count < 10):
+				count = count + 1
+				try:
+					q = requests.get("http://nsoud.cz"+link,allow_redirects=True)
+				except Exception as e:
+					print("{} - {}".format(matchObj, e))
+				if q.status == 200:
+					break
+				else:
+					time.sleep(5)
 			root = etree.HTML(q.text)
 			a = root.xpath('//div[@id="content"]')[0]
 			content = tostring(a, method='html', encoding="UTF-8").decode("utf-8")
