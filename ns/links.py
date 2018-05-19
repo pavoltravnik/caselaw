5a635f864063a8a16281607c462fec3071931034
Pavol Travnik <pavol.travnik@gmail.com>
2018-05-19 12:40:07 +0200

    new parser najvyssi soud

--- ns/links.py
+++ ns/links.py
@@ -10,39 +10,43 @@ import lxml.html
 from lxml import etree
 from lxml.etree import tostring
 import os
-import argparse
 import multiprocessing
+import calendar
+import argparse
 
-path="documents"
+parser = argparse.ArgumentParser()
+parser.add_argument("--month", help="start month", type=int)
+parser.add_argument("--year", help="start year", type=int)
+args = parser.parse_args()
+
+
+path="ns/documents"
 if not os.path.exists(path):
 	os.makedirs(path)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--sday", help="start month", type=int)
-parser.add_argument("--smonth", help="start month", type=int)
-parser.add_argument("--syear", help="start year", type=int)
-parser.add_argument("--eday", help="end month", type=int)
-parser.add_argument("--emonth", help="end month", type=int)
-parser.add_argument("--eyear", help="end year", type=int)
-args = parser.parse_args()
-
-address = "http://nsoud.cz/Judikatura/judikatura_ns.nsf/$$WebSearch1?SearchView&Query=%5Bdatum_predani_na_web%5D%3E%3D{0}%2F{1}%2F{2}%20AND%20%5Bdatum_predani_na_web%5D%3C%3D{3}%2F{4}%2F{5}&SearchOrder=2&SearchMax=0&Start=1&Count=1000&pohled=1".format(str(args.sday),str(args.smonth),str(args.syear),str(args.eday),str(args.emonth),str(args.eyear))
-
-r = requests.get(address,allow_redirects=True)
-root = lxml.html.fromstring(r.text)
-links = root.xpath("//a/@href[starts-with(.,'/Judikatura/judikatura_ns.nsf/WebSearch')]")
-
-print("Found {} links.".format(len(links)))
-
-if len(links) > 990:
-	raise Exception('Too many links in this period. Try to find another time period.')
+def parse_month(parse_year,parse_month):
+	for day in range(1, calendar.monthrange(parse_year,parse_month)[1]):
+		parse_decisions(parse_year,parse_month,day)
 
 
-# Write down links
-for link in links:
-	with open("links.txt", "a") as myfile:
-		myfile.write(link+"\n")
+def parse_decisions(year,month,day):
+	address = "http://nsoud.cz/Judikatura/judikatura_ns.nsf/$$WebSearch1?SearchView&Query=%5Bdatum_predani_na_web%5D%3E%3D{0}%2F{1}%2F{2}%20AND%20%5Bdatum_predani_na_web%5D%3C%3D{3}%2F{4}%2F{5}&SearchOrder=2&SearchMax=0&Start=1&Count=1000&pohled=1".format(str(day),str(month),str(year),str(day),str(month),str(year))
+	r = requests.get(address,allow_redirects=True)
+	root = lxml.html.fromstring(r.text)
+	links = root.xpath("//a/@href[starts-with(.,'/Judikatura/judikatura_ns.nsf/WebSearch')]")
+	if len(links) > 990:
+		raise Exception('Too many links in this period ({}). {}-{}-{}'.format(len(links),day,month,year))
 
+	# Write down links
+	for link in links:
+		with open("ns/links.txt", "a") as myfile:
+			myfile.write(link+"\n")
+
+	pool = multiprocessing.Pool(processes=8)
+	pool_outputs = pool.map(get_content,links)
+	pool.close()
+	pool.join()
+	print("{}\t{}\t{}-{}-{}".format(len(links),len(pool_outputs),day,month,year))
 
 def get_content(link):
 	try:
@@ -52,17 +56,14 @@ def get_content(link):
 			root = etree.HTML(q.text)
 			a = root.xpath('//div[@id="content"]')[0]
 			content = tostring(a, method='html', encoding="UTF-8").decode("utf-8")
-			with open("documents/"+str(matchObj.group(1)+".html"), "a") as f:
+			with open("ns/documents/"+str(matchObj.group(1)+".html"), "a") as f:
 				f.write(content)
 		else:
 			raise Exception('Parsing weblink error!')
 	except Exception as e:
 		raise Exception(str(e))
 
-
-pool = multiprocessing.Pool(processes=8)
-pool_outputs = pool.map(get_content,links)
-pool.close()
-pool.join()
-
-print("Saved {} documents.".format(len(pool_outputs)))
+if __name__ == '__main__':
+    # service.py executed as script
+    # do something
+    parse_decisions(args.year, args.month)
