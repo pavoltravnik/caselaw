f6620c963f04f3ad32934c2a152367ceb0604345
Pavol Travnik <pavol.travnik@gmail.com>
2018-05-28 19:44:27 +0200

    indexer updated ES

--- ns/index.py
+++ ns/index.py
@@ -1,27 +1,39 @@
-#!/usr/bin/python
-# requirements: "pip install lxml elasticsearch"
-# usage: "python3 ns/links.py --month 1 --year 2018"
-
 import lxml.etree
 import lxml.html
 from lxml import etree
 from lxml.etree import tostring
 
-
+import os
 from os import listdir
 from os.path import isfile, join
-saved_documents = [f for f in listdir("ns/documents") if isfile(join("ns/documents", f))]
+
+if not os.path.exists("ns/indexed.txt"):
+	open("ns/indexed.txt", "w+")
+else:
+	with open("ns/indexed.txt", "r") as f:
+		indexed_docs = f.readlines()
+
+indexed_docs = [x.strip() for x in indexed_docs] 
+
+saved_documents = list(set([f for f in listdir("ns/documents") if isfile(join("ns/documents", f))]) - set(indexed_docs))
+
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
 
 
 def get_text(xpath,root,doc):
 	if (len(root.xpath(xpath)) == 1):
-		value = root.xpath(xpath)[0].text_content()
-		return value
+		return root.xpath(xpath)[0].text_content()
 	elif (len(root.xpath(xpath)) > 1):
 		raise Exception('Multiple mathes for {} - {}'.format(xpath,doc))
 	else:
 		return None
 
+def get_list(xpath,root,doc):
+	if get_text(xpath,root,doc):
+		return [x.strip() for x in get_text(xpath,root,doc).splitlines()]
+	else:
+		return None
 
 for doc in saved_documents:
 	with open('ns/documents/'+doc, 'r') as myfile:
@@ -29,14 +41,14 @@ for doc in saved_documents:
 	root = lxml.html.fromstring(document_content)
 	for bad in root.xpath("//p[@class='fright']"):
 		bad.getparent().remove(bad)
-	soud = get_text("//tr[td='Soud:']/td[2]", root,doc)
+	soud = get_text("//table[@id='box-table-a']//tr[td='Soud:']/td[2]", root,doc)
 	datum = get_text("//tr[td='Datum rozhodnutí:']/td[2]", root,doc)
-	spzn =  [x.strip() for x in get_text("//tr[td='Spisová značka:']/td[2]", root,doc).splitlines()]
+	spzn =  get_list("//tr[td='Spisová značka:']/td[2]", root,doc)
 	ecli = get_text("//tr[td='ECLI:']/td[2]", root,doc)
-	typ = [x.strip() for x in get_text("//tr[td='Typ rozhodnutí:']/td[2]", root,doc).splitlines()]
-	katego = [x.strip() for x in get_text("//tr[td='Kategorie rozhodnutí:']/td[2]", root,doc).splitlines()]
-	heslo = [x.strip() for x in get_text("//tr[td='Heslo:']/td[2]", root,doc).splitlines()]
-	predpis = [x.strip() for x in get_text("//tr[td='Dotčené předpisy:']/td[2]", root,doc).splitlines()]
+	typ = get_list("//tr[td='Typ rozhodnutí:']/td[2]", root,doc)
+	katego = get_list("//tr[td='Kategorie rozhodnutí:']/td[2]", root,doc)
+	heslo = get_list("//tr[td='Heslo:']/td[2]", root,doc)
+	predpis = get_list("//tr[td='Dotčené předpisy:']/td[2]", root,doc)
 	duvod = get_text("//tr[td='Důvod dovolání:']/td[2]", root,doc)
 	veta = get_text("//tr[td='Právní věta:']/td[2]", root,doc)
 	sbirka = get_text("//tr[td='Publikováno ve sbírce pod číslem:']/td[2]", root,doc)
@@ -61,3 +73,24 @@ for doc in saved_documents:
 		content_html = tostring(content[0], method='html', encoding="UTF-8").decode("utf-8")
 	else:
 		raise Exception('No content was found - {}'.format(doc))
+	documentES = {
+	'soud': soud,
+	'datum': datum,
+	'spzn': spzn,
+	'ecli': ecli,
+	'typ': typ,
+	'katego':katego,
+	'heslo': heslo,
+	'predpis': predpis,
+	'duvod': duvod, 
+	'veta': veta,
+	'sbirka': sbirka,
+	'nazev': nazev,
+	'senat': senat,
+	'us_json': us_json,
+	'content': content_text
+	}
+	res = es.index(index="my_index", doc_type='nsoud', id=doc, body=documentES)
+	if res['result'] == "created" or res['result'] == "updated":
+		with open("ns/indexed.txt", "a") as f:
+			f.write(doc+"\n")
