03f93aee198f34fd49a3bed5351e0c758918e199
Pavol Travnik <pavol.travnik@gmail.com>
2018-05-28 19:15:34 +0200

    index all

--- ns/clean.py
+++ ns/clean.py
@@ -10,8 +10,12 @@ saved_documents = [f for f in listdir("ns/documents") if isfile(join("ns/documen
 
 
 for doc in saved_documents:
-	with open('ns/documents/'+doc, 'r') as myfile:
-		document_content=myfile.read()
+	try:
+		with open('ns/documents/'+doc, 'r') as myfile:
+			document_content=myfile.read()
+	except:
+		print("ERROR "+doc+"\n")
+		os.remove('ns/documents/'+doc)
 	try:
 		root = lxml.html.fromstring(document_content)
 	except:
