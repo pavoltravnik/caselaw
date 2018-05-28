55ba4874746b88fa930a52742850d53e87ee16ae
Pavol Travnik <pavol.travnik@gmail.com>
2018-05-28 19:01:28 +0200

    remove all unreadable

--- ns/clean.py
+++ ns/clean.py
@@ -6,13 +6,21 @@ from lxml.etree import tostring
 import os
 from os import listdir
 from os.path import isfile, join
-saved_documents = [f for f in listdir("ns/documents") if isfile(join("ns/documents", f))]
+saved_documents = [f for f in listdir("ns/documents") if isfile(join("ns/documents", f))][:5000]
 
 
 for doc in saved_documents:
 	with open('ns/documents/'+doc, 'r') as myfile:
 		document_content=myfile.read()
-	root = lxml.html.fromstring(document_content)
-	if (len(root.xpath("//div[@class='main_detail']")) > 1):
-		print('Multiple mathes for {} - {}'.format("//div[@class='main_detail']",doc))
+	try:
+		root = lxml.html.fromstring(document_content)
+	except:
+		print("ERROR "+doc+"\n")
+		os.remove('ns/documents/'+doc)
+	try:
+		if (len(root.xpath("//div[@class='main_detail']")) > 1):
+			print('Multiple mathes for {} - {}'.format("//div[@class='main_detail']",doc))
+			os.remove('ns/documents/'+doc)
+	except:
+		print("ERROR "+doc+"\n")
 		os.remove('ns/documents/'+doc)
